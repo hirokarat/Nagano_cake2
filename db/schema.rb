3f6976f96b2d017242d1b6236c6f80186fdadedd
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-09-05 06:41:58 +0000

    編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_055613) do
+ActiveRecord::Schema.define(version: 2022_09_05_062743) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2022_09_05_055613) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
-    t.boolean "is_active", default: false, null: false
+    t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
